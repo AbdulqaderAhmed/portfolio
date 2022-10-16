db10d7bdd9f6802bc4f14f9ae643a72a2af7c4a2
AbdulqaderAhmed <abdulqaderahmed32@gmail.com>
2022-10-16 23:53:32 +0300

    link icons added

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -7,6 +7,7 @@ import Movie3 from "../../assets/image/movie3.png";
 import Nile from "../../assets/image/Nile.png";
 import Resturant from "../../assets/image/resturant.png";
 import Tenawo from "../../assets/image/Tenawo.png";
+import { FaLink } from "react-icons/fa";
 import AOS from "aos";
 
 export default function Project() {
@@ -28,7 +29,12 @@ export default function Project() {
           >
             <img src={Blog} alt="blog" className="" />
             <div className="p-6 hover:bg-pink-700">
-              <h3 className="text-xl font-medium">Blog</h3>
+              <div className="flex justify-between">
+                <h3 className="text-xl font-medium">Blog</h3>
+                <a className="pt-1">
+                  <FaLink className="cursor-pointer" />
+                </a>
+              </div>
               <p className="text-gray-300 text-sm py-5">
                 This project is done by laravel framework and helps people to
                 read blogs.
@@ -44,7 +50,12 @@ export default function Project() {
           >
             <img src={Chat} alt="chat" className="" />
             <div className="p-6 hover:bg-pink-700">
-              <h3 className="text-xl font-medium">Let's Chat</h3>
+              <div className="flex justify-between">
+                <h3 className="text-xl font-medium">Let's Chat</h3>
+                <a className="pt-1">
+                  <FaLink className="cursor-pointer" />
+                </a>
+              </div>
               <p className="text-gray-300 text-sm py-5">
                 Helps freinds to chat each other, built using laravel and
                 authenticate using social medias.
@@ -60,7 +71,12 @@ export default function Project() {
           >
             <img src={Ecom} alt="ecommerce" className="" />
             <div className="p-6 hover:bg-pink-700">
-              <h3 className="text-xl font-medium">E-commerce</h3>
+              <div className="flex justify-between">
+                <h3 className="text-xl font-medium">E-commerce</h3>
+                <a className="pt-1">
+                  <FaLink className="cursor-pointer" />
+                </a>
+              </div>
               <p className="text-gray-300 text-sm py-5">
                 E-commerce dashboard used to manage e-commerce for admins and
                 buying for customers. built using Laravel framework.
@@ -76,7 +92,12 @@ export default function Project() {
           >
             <img src={Employee} alt="employee" className="" />
             <div className="p-6 hover:bg-pink-700">
-              <h3 className="text-xl font-medium">Employee Managment</h3>
+              <div className="flex justify-between">
+                <h3 className="text-xl font-medium">Employee Management</h3>
+                <a className="pt-1">
+                  <FaLink className="cursor-pointer" />
+                </a>
+              </div>
               <p className="text-gray-300 text-sm py-5">
                 Helps a company to manage their company's employee. Built using
                 laravel API with React Js.
@@ -92,7 +113,12 @@ export default function Project() {
           >
             <img src={Movie3} alt="movie" className="" />
             <div className="p-6 hover:bg-pink-700">
-              <h3 className="text-xl font-medium">Ocean Of Movies</h3>
+              <div className="flex justify-between">
+                <h3 className="text-xl font-medium">Ocean Of Movies</h3>
+                <a className="pt-1">
+                  <FaLink className="cursor-pointer" />
+                </a>
+              </div>
               <p className="text-gray-300 text-sm py-5">
                 Entertainment hub, which is used to get latest movies, series,
                 tv shows in one place. Built using TMDB api with React JS and
@@ -109,9 +135,14 @@ export default function Project() {
           >
             <img src={Nile} alt="nile" className="" />
             <div className="p-6 hover:bg-pink-700">
-              <h3 className="text-xl font-medium">
-                Nile Insurance Car Price System
-              </h3>
+              <div className="flex justify-between">
+                <h3 className="text-xl font-medium">
+                  Nile Insurance Car Price System
+                </h3>
+                <a className="pt-1">
+                  <FaLink className="cursor-pointer" />
+                </a>
+              </div>
               <p className="text-gray-300 text-sm py-5">
                 Nile insurance Car price indication system. built using laravel
                 framework.
@@ -127,7 +158,12 @@ export default function Project() {
           >
             <img src={Resturant} alt="resturant" className="" />
             <div className="p-6 hover:bg-pink-700">
-              <h3 className="text-xl font-medium">Side-Hustle Resturant</h3>
+              <div className="flex justify-between">
+                <h3 className="text-xl font-medium">Side-hustle Resturant</h3>
+                <a className="pt-1">
+                  <FaLink className="cursor-pointer" />
+                </a>
+              </div>
               <p className="text-gray-300 text-sm py-5">
                 Side-hustle project which is built using React Js with Material
                 UI.
@@ -143,7 +179,12 @@ export default function Project() {
           >
             <img src={Tenawo} alt="tenawo" className="" />
             <div className="p-6 hover:bg-pink-700">
-              <h3 className="text-xl font-medium">Tenawo</h3>
+              <div className="flex justify-between">
+                <h3 className="text-xl font-medium">Tenawo</h3>
+                <a className="pt-1">
+                  <FaLink className="cursor-pointer" />
+                </a>
+              </div>
               <p className="text-gray-300 text-sm py-5">
                 Helps save your time, client's find drugs and services within
                 500 meters radius, built with react js.
